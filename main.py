2f967bc792a0369993f6ff2156f84588f769f5e5
Letícia Roque Lima Oliveira <62360470+letty-Rock@users.noreply.github.com>
2023-03-22 15:16:23 -0300

    ..

--- main.py
+++ main.py
@@ -10,21 +10,14 @@ co2 = "#ff843d"  #orange
 co3 = "#334acc"  
 co4 = "#e1e4f2"  #ash
 
-all_values = ""
-text_values = StringVar()
-
-def entering_values(number):
-   global all_values
-
-   all_values = all_values + str(number)
-   text_value.set(all_values)
-
 window = Tk ()
 window.title ("Calculator")
 window.geometry ('235x318')
 window.configure(bg=co1)
+window.mainloop ()
 
 ttk.Separator(window, orient=HORIZONTAL).grid(row=0, columnspan=1, ipadx=280)
+
 
 frame_score = Frame(window, width=300, height=56, bg=co1, pady=0, padx=0)
 frame_score.grid(row=1, column=0, sticky=NW)
@@ -32,8 +25,9 @@ frame_score.grid(row=1, column=0, sticky=NW)
 frame_buttons = Frame(window, width=300, height=340, bg=co1, pady=0, padx=0)
 frame_buttons.grid(row=2, column=0, sticky=NW)
 
-app_screen = Label(frame_score, width=16, height=2, textvariable=text_value, padx=7, archor="e", bd=0, justify=RIGHT, font=('Ivy 18'), bg=co1, fg=co0)
+app_screen = Label(frame_score, width=16, height=2, textvariable=text_value, padx=7, anchor="e", bd=0, justify=RIGHT, font=('Ivy 18'), bg=co1, fg=co0)
 app_screen.place(x=0, y=0)
+
 
 
 b_1 = Button(frame_buttons, text="C", command=lambda:clear_screen(), width=11, height=2, bg=co4, fg=co1, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
@@ -78,11 +72,33 @@ b_18 = Button(frame_buttons, text="=", command = lambda:calculate(), width=5, he
 b_18.place(x=177, y=208)
 
 
+all_values = ""
+text_value = StringVar()
+
+
+
+def entering_values(number):
+   global all_values
+
+   all_values = all_values + str(number)
+   text_value.set(all_values)
+
+
+def calculate():
+    global all_values
+    result = str(eval(all_values))
+    text_value.set(result)
+    all_values = ""
 
 
 
 
 
+def clear_screen():
+    global all_values
+    all_values = ""
+    text_value.set("")
 
 
-window.mainloop ()
+
+
